0525e6264926d1664433d58a8d8683e2a9a9bd3e
Raphael Beck <raphael.beck@glitchedpolygons.com>
2024-07-23 03:02:01 +0200

    Initial commit.

---
+++ GlitchedPolygons.GlitchAES.BlazorWebApp/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace GlitchedPolygons.GlitchAES.BlazorWebApp;
+
+public static class Constants
+{
+    public static class Bool
+    {
+        public const string TRUE = "true";
+        public const string FALSE = "false";
+    }
+    
+    public static class PreferenceIds
+    {
+        public const string DARK_THEME = "DarkTheme";
+    }
+
+    public static class AesGcm
+    {
+        public const int NONCE_SIZE_BYTES = 12;
+        public const int MAC_SIZE_BYTES = 16;
+        public const int TAG_SIZE_BYTES = 16;
+        public const int KEY_SIZE_BYTES = 32;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# GlitchAES
+
+## Tha glitches, m8!
+
+## PWA GUI
+
+GlitchAES (say: "glitches") is a web-based (zero-knowledge, static client-side only) GUI that wraps AES-GCM.
+
+You can encrypt and decrypt messages using either AES-128, AES-192 or AES-256 (in Galois-Counter mode).
